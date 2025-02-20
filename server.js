9a2dbed6fe82c033864a9e20a162ef4562fc2448
Dior1122 <diortopik2020@gmail.com>
2025-02-20 12:12:11 +0900

    fix: modified harid.js

--- server.js
+++ server.js
@@ -14,12 +14,26 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routingcode
-app.get("/hello", function (req, res) {
-    res.end(`<h1>HELLO WORD</h1>`);
-});
-app.get("/gift", function (req, res) {
-    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-});
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
+  });
+  
+  // get datebas dan malumotni uqish uchun
+  app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
+  });
+  
+  app.get("/", function (req, res) {
+    res.render("harid");
+  });
+// app.get("/hello", function (req, res) {
+//     res.end(`<h1>HELLO WORD</h1>`);
+// });
+// app.get("/gift", function (req, res) {
+//     res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+// });
 
 const server = http.createServer(app);
 let PORT = 3000;

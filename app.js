7d73c11d0224b13d16b578e161aab8ad64400d75
Dior1122 <diortopik2020@gmail.com>
2025-03-03 07:53:04 +0900

    feat: FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -33,13 +33,9 @@ app.set("view engine", "ejs");
     console.log(req.body);
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-      if (err) {
-        console.log(err);
-        res.end("something went wrong");
-      } else {
-        res.end("successfully added");
-      }
-    })             //res.end("success"); // res.json({test: "success"});
+      console.log(data.ops);
+      res.json(data.ops[0]); 
+    });            //res.end("success"); // res.json({test: "success"});
   });
 
   app.get('/author', (req, res) => {
